070167a584d3335a3dfa2861ef430115a4a86e3f
yosyes37-arch <yosyes37@gmail.com>
2025-12-04 19:00:05 +0700

    Create unpr6.sh

---
+++ unpr6.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+REMOTE_PATH="/var/www/pterodactyl/app/Http/Controllers/Admin/Settings/IndexController.php"
+
+echo "🚀 Menghapus proteksi Anti Akses Settings..."
+
+mkdir -p "$(dirname "$REMOTE_PATH")"
+chmod 755 "$(dirname "$REMOTE_PATH")"
+
+cat > "$REMOTE_PATH" << 'EOF'
+<?php
+
+namespace Pterodactyl\Http\Controllers\Admin\Settings;
+
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Prologue\Alerts\AlertsMessageBag;
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\View\Factory as ViewFactory;
+use Pterodactyl\Http\Controllers\Controller;
+use Pterodactyl\Traits\Helpers\AvailableLanguages;
+use Pterodactyl\Services\Helpers\SoftwareVersionService;
+use Pterodactyl\Contracts\Repository\SettingsRepositoryInterface;
+use Pterodactyl\Http\Requests\Admin\Settings\BaseSettingsFormRequest;
+
+class IndexController extends Controller
+{
+    use AvailableLanguages;
+
+    /**
+     * IndexController constructor.
+     */
+    public function __construct(
+        private AlertsMessageBag $alert,
+        private Kernel $kernel,
+        private SettingsRepositoryInterface $settings,
+        private SoftwareVersionService $versionService,
+        private ViewFactory $view
+    ) {
+    }
+
+    /**
+     * Render the UI for basic Panel settings.
+     */
+    public function index(): View
+    {
+        return $this->view->make('admin.settings.index', [
+            'version' => $this->versionService,
+            'languages' => $this->getAvailableLanguages(true),
+        ]);
+    }
+
+    /**
+     * Handle settings update.
+     *
+     * @throws \Pterodactyl\Exceptions\Model\DataValidationException
+     * @throws \Pterodactyl\Exceptions\Repository\RecordNotFoundException
+     */
+    public function update(BaseSettingsFormRequest $request): RedirectResponse
+    {
+        foreach ($request->normalize() as $key => $value) {
+            $this->settings->set('settings::' . $key, $value);
+        }
+
+        $this->kernel->call('queue:restart');
+        $this->alert->success('Panel settings have been updated successfully and the queue worker was restarted to apply these changes.')->flash();
+
+        return redirect()->route('admin.settings');
+    }
+}
+
+EOF
+
+chmod 644 "$REMOTE_PATH"
+
+echo "✅ Menghapus Proteksi Anti Akses Settings berhasil di hapus!"
+echo "📂 Lokasi file: $REMOTE_PATH"
+echo "🔒 Berhasil 100%."
