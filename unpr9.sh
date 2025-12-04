f832f28d72b98be9fe89532af8653e67c0ad705e
yosyes37-arch <yosyes37@gmail.com>
2025-12-04 19:03:02 +0700

    Create unpr9.sh

---
+++ unpr9.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+REMOTE_PATH="/var/www/pterodactyl/app/DetailsModificationService.php"
+
+echo "🚀 Menghapus proteksi Anti Modifikasi Detail Server..."
+
+mkdir -p "$(dirname "$REMOTE_PATH")"
+chmod 755 "$(dirname "$REMOTE_PATH")"
+
+cat > "$REMOTE_PATH" << 'EOF'
+<?php
+
+namespace Pterodactyl\Services\Servers;
+
+use Illuminate\Support\Arr;
+use Pterodactyl\Models\Server;
+use Illuminate\Database\ConnectionInterface;
+use Pterodactyl\Traits\Services\ReturnsUpdatedModels;
+use Pterodactyl\Repositories\Wings\DaemonServerRepository;
+use Pterodactyl\Exceptions\Http\Connection\DaemonConnectionException;
+
+class DetailsModificationService
+{
+    use ReturnsUpdatedModels;
+
+    /**
+     * DetailsModificationService constructor.
+     */
+    public function __construct(private ConnectionInterface $connection, private DaemonServerRepository $serverRepository)
+    {
+    }
+
+    /**
+     * Update the details for a single server instance.
+     *
+     * @throws \Throwable
+     */
+    public function handle(Server $server, array $data): Server
+    {
+        return $this->connection->transaction(function () use ($data, $server) {
+            $owner = $server->owner_id;
+
+            $server->forceFill([
+                'external_id' => Arr::get($data, 'external_id'),
+                'owner_id' => Arr::get($data, 'owner_id'),
+                'name' => Arr::get($data, 'name'),
+                'description' => Arr::get($data, 'description') ?? '',
+            ])->saveOrFail();
+
+            // If the owner_id value is changed we need to revoke any tokens that exist for the server
+            // on the Wings instance so that the old owner no longer has any permission to access the
+            // websockets.
+            if ($server->owner_id !== $owner) {
+                try {
+                    $this->serverRepository->setServer($server)->revokeUserJTI($owner);
+                } catch (DaemonConnectionException $exception) {
+                    // Do nothing. A failure here is not ideal, but it is likely to be caused by Wings
+                    // being offline, or in an entirely broken state. Remember, these tokens reset every
+                    // few minutes by default, we're just trying to help it along a little quicker.
+                }
+            }
+
+            return $server;
+        });
+    }
+}
+
+EOF
+
+chmod 644 "$REMOTE_PATH"
+
+echo "✅ Menghapus Proteksi Anti Modifikasi Detail Server berhasil di hapus!"
+echo "📂 Lokasi file: $REMOTE_PATH"
+echo "🔒 Berhasil 100%."
