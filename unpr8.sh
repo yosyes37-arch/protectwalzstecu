e8b36cd4879839a1155a4537c6a8b37958b51d90
yosyes37-arch <yosyes37@gmail.com>
2025-12-04 19:01:57 +0700

    Create unpr8.sh

---
+++ unpr8.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+REMOTE_PATH="/var/www/pterodactyl/app/Http/Controllers/Api/Client/Servers/ServerController.php"
+
+echo "🚀 Menghapus proteksi Anti Akses Server 2..."
+
+mkdir -p "$(dirname "$REMOTE_PATH")"
+chmod 755 "$(dirname "$REMOTE_PATH")"
+
+cat > "$REMOTE_PATH" << 'EOF'
+<?php
+
+namespace Pterodactyl\Http\Controllers\Api\Client\Servers;
+
+use Pterodactyl\Models\Server;
+use Pterodactyl\Transformers\Api\Client\ServerTransformer;
+use Pterodactyl\Services\Servers\GetUserPermissionsService;
+use Pterodactyl\Http\Controllers\Api\Client\ClientApiController;
+use Pterodactyl\Http\Requests\Api\Client\Servers\GetServerRequest;
+
+class ServerController extends ClientApiController
+{
+    /**
+     * ServerController constructor.
+     */
+    public function __construct(private GetUserPermissionsService $permissionsService)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Transform an individual server into a response that can be consumed by a
+     * client using the API.
+     */
+    public function index(GetServerRequest $request, Server $server): array
+    {
+        return $this->fractal->item($server)
+            ->transformWith($this->getTransformer(ServerTransformer::class))
+            ->addMeta([
+                'is_server_owner' => $request->user()->id === $server->owner_id,
+                'user_permissions' => $this->permissionsService->handle($server, $request->user()),
+            ])
+            ->toArray();
+    }
+}
+
+EOF
+
+chmod 644 "$REMOTE_PATH"
+
+echo "✅ Menghapus Proteksi Anti Akses Server 2 berhasil di hapus!"
+echo "📂 Lokasi file: $REMOTE_PATH"
+echo "🔒 Berhasil 100%."
